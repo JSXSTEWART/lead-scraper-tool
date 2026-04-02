1b9408b1b8514a3173316688ca7f55c99b01bdcc
JSXSTEWART <91479502+JSXSTEWART@users.noreply.github.com>
2026-04-02 18:54:44 -0500

    Add Launch Control Nuxt config

---
+++ launch-control-demo/nuxt.config.ts
@@ -0,0 +1,27 @@
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxthub/core',
+    'nuxt-auth-utils',
+    '@pinia/nuxt',
+    '@pinia/colada-nuxt'
+  ],
+  devtools: {
+    enabled: true
+  },
+  css: ['~/assets/main.css'],
+  future: { compatibilityVersion: 4 },
+  compatibilityDate: '2025-08-07',
+  hub: {
+    db: 'sqlite'
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        quotes: 'single',
+        commaDangle: 'never'
+      }
+    }
+  }
+})
